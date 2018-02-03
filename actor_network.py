67b616fe4af9372b368994eed24f21644d9dd183
albHun <qj213@cam.ac.uk>
2018-02-03 15:56:55 +0000

    added actor, critic and target networks

---
+++ actor_network.py
@@ -0,0 +1,44 @@
+from mxnet import nd
+from mxnet.gluon import nn
+import mxnet as mx
+
+import numpy as np
+import copy
+
+
+class ActorNetwork:
+    def __init__(self, output_dim, ctx=mx.cpu()):
+        """
+
+        :type output_dim: int
+        :type ctx: mx.Context
+        """
+        self.ctx = ctx
+        self.output_dim = output_dim
+        self.net = self.get_net()
+
+    def get_net(self):
+        net = nn.Sequential()
+        with net.name_scope():
+            net.add(nn.Dense(self.output_dim))
+        net.initialize(init=mx.init.Xavier(), ctx=self.ctx)
+        return net
+
+
+class TargetNetMu:
+    def __init__(self, actor, ctx=mx.cpu()):
+        # Initialize target network with the same structure and weights as actor network
+        self.net = copy.copy(actor.net)
+        self.ctx = ctx
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    mx.random.seed(0)
+
+    act_net = ActorNetwork(output_dim=10)
+    tar_net = TargetNetMu(act_net)
+    print(act_net.net(nd.array([1])))
+    print(tar_net.net(nd.array([1])))
+    print(act_net.net is tar_net.net)
+
---
+++ critic_network.py
@@ -0,0 +1,42 @@
+from mxnet import nd
+from mxnet.gluon import nn
+import mxnet as mx
+
+import numpy as np
+import copy
+
+
+class CriticNetwork:
+    def __init__(self, output_dim, ctx=mx.cpu()):
+        """
+
+        :type ctx: mx.Context
+        """
+        self.ctx = ctx
+        self.output_dim = output_dim
+        self.net = self.get_net()
+
+    def get_net(self):
+        net = nn.Sequential()
+        with net.name_scope():
+            net.add(nn.Dense(self.output_dim))
+        net.initialize(init=mx.init.Xavier(), ctx=self.ctx)
+        return net
+
+
+class TargetNetQ:
+    def __init__(self, critic, ctx=mx.cpu()):
+        # Initialize target network with the same structure and weights as actor network
+        self.net = copy.copy(critic.net)
+        self.ctx = ctx
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    mx.random.seed(0)
+
+    cri_net = CriticNetwork(output_dim=10)
+    tar_net = TargetNetQ(cri_net)
+    print(cri_net.net(nd.array([1])))
+    print(tar_net.net(nd.array([1])))
+    print(cri_net.net is tar_net.net)
