d30fb9bfd78d436031b62374c2dcc845f46856a8
albHun <qj213@cam.ac.uk>
2018-02-06 19:47:18 +0000

    modified state shape to allow computation

--- ddpg/train.py
+++ ddpg/train.py
@@ -15,6 +15,8 @@ M = 70  # outer loop iteration
 T = 400  # inner loop iteration
 tau = 0.01  # update lagging coefficient
 gamma = 0.99  # discount factor
+buffer_size = 5000  # buffer capacity
+
 
 ENV_NAME = 'Pendulum-v0'
 
@@ -30,38 +32,57 @@ if __name__ == "__main__":
     action_dim = env.action_space.shape[0]
 
     # Initialize actor, critic and target networks
-    actor = ActorNetwork(action_dim)
+    actor = ActorNetwork(action_dim=action_dim)
     critic = CriticNetwork()
-    target_a = TargetNetMu(actor)
-    target_mu = TargetNetQ(critic)
-
-
-
-
-
-
-
-
-
-
-
-
-    # Initialize the actor, critic and two target networks
-    actor = ActorNetwork(1)
-    critic = CriticNetwork(1)
     target_mu = TargetNetMu(actor)
-    tar_q = TargetNetQ(critic)
+    target_q = TargetNetQ(critic)
 
-    # Test whether parameters are copied correctly
-    test_input = nd.array([1])
-    assert nd.equal(actor.net(test_input), target_mu.net(test_input))[0][0] == 1.
-    assert nd.equal(critic.net(test_input), tar_q.net(test_input))[0][0] == 1.
+    # Testing network initializations
+    # test_array = nd.array([1.])
+    # assert actor.net(test_array)[0][0] == -1.2251712
+    # assert target_mu.net(test_array)[0][0] == -1.2251712
+    # assert critic.net(test_array)[0][0] == -0.9443339
+    # assert target_q.net(test_array)[0][0] == -0.9443339
 
-    # Initialize memory buffer
-    memory = Memory(capacity=7000, dims=8)
+    # Initialize buffer
+    memory = Memory(capacity=buffer_size, dims=2*state_dim + action_dim + 1)
 
-    for m in range(M):
+    # Outer iteration
+    for i in range(M):
 
-        # Start new environment
-        # To do: inner implementation of DDPG
-        pass
+        # Receive initial observation
+        s = env.reset()
+        explore_variance = 3  # initial exploration variance
+
+        s = nd.array(s).reshape((1, -1))
+        # Inner iteration
+        for j in range(T):
+            action = actor.net(s)
+
+
+
+
+
+
+
+
+
+    # # Initialize the actor, critic and two target networks
+    # actor = ActorNetwork(1)
+    # critic = CriticNetwork(1)
+    # target_mu = TargetNetMu(actor)
+    # tar_q = TargetNetQ(critic)
+    #
+    # # Test whether parameters are copied correctly
+    # test_input = nd.array([1])
+    # assert nd.equal(actor.net(test_input), target_mu.net(test_input))[0][0] == 1.
+    # assert nd.equal(critic.net(test_input), tar_q.net(test_input))[0][0] == 1.
+    #
+    # # Initialize memory buffer
+    # memory = Memory(capacity=7000, dims=8)
+    #
+    # for m in range(M):
+    #
+    #     # Start new environment
+    #     # To do: inner implementation of DDPG
+    #     pass
