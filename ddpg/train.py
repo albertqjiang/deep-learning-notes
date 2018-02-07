b2809c56f658e000b69203b42d2930b1ac9d875d
albHun <qj213@cam.ac.uk>
2018-02-07 22:46:00 +0000

    remove useless comments

--- ddpg/train.py
+++ ddpg/train.py
@@ -93,26 +93,3 @@ if __name__ == "__main__":
 
                 y = r + q_target * gamma
                 # print(y)
-
-
-
-
-    # # Initialize the actor, critic and two target networks
-    # actor = ActorNetwork(1)
-    # critic = CriticNetwork(1)
-    # target_mu = TargetNetMu(actor)
-    # tar_q = TargetNetQ(critic)
-    #
-    # # Test whether parameters are copied correctly
-    # test_input = nd.array([1])
-    # assert nd.equal(actor.net(test_input), target_mu.net(test_input))[0][0] == 1.
-    # assert nd.equal(critic.net(test_input), tar_q.net(test_input))[0][0] == 1.
-    #
-    # # Initialize memory buffer
-    # memory = Memory(capacity=7000, dims=8)
-    #
-    # for m in range(M):
-    #
-    #     # Start new environment
-    #     # To do: inner implementation of DDPG
-    #     pass
